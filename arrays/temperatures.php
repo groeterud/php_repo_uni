80ecb55cd4dcf0022169418dc1fc78ea1abff2ae
groeterud <groeterud@users.noreply.github.com>
2021-10-18 11:05:30 +0200

    ezpz
    Skulle holdt meg hjemme

--- arrays/temperatures.php
+++ arrays/temperatures.php
@@ -3,12 +3,10 @@ $temps = array (78, 60, 62, 68, 71, 68, 73, 85, 66, 64, 76, 63, 75, 76, 73, 68, 
 74, 62, 62, 65, 64, 68, 73, 75, 79, 73);
 
 $avg_temp = array_sum($temps) / count($temps);
-
 echo "The average temperature is: ".number_format($avg_temp,1);
 
 //sort ascending order
 sort($temps);
-
 echo "The five lowest temperatures is: ".$temps[0]. ", " .$temps[1].", ".$temps[2].", ".$temps[3].", ".$temps[4];
 
 //sort desc
