b99a68f913422ba0816921c23b06f87c44e0f138
groeterud <groeterud@users.noreply.github.com>
2021-10-18 11:47:48 +0200

    Update temperatures.php
    jassssss

--- arrays/temperatures.php
+++ arrays/temperatures.php
@@ -2,8 +2,23 @@
 $temps = array (78, 60, 62, 68, 71, 68, 73, 85, 66, 64, 76, 63, 75, 76, 73, 68, 62, 73, 72, 65,
 74, 62, 62, 65, 64, 68, 73, 75, 79, 73);
 
+/* smart way
 $avg_temp = array_sum($temps) / count($temps);
+*/
+
+//other way
+$length=0;
+$sum=0;
+
+foreach($temps as $temp) {
+    $sum += $temp;
+    $length++;
+}
+$avg_temp = $sum / $length;
 echo "The average temperature is: ".number_format($avg_temp,1);
+
+//remove duplicates
+$temps = array_unique($temps);
 
 //sort ascending order
 sort($temps);
